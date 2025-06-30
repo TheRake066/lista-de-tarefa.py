c803fbe486026a5159e7e4049454380e88f74d58
The Rake Dev <sidneyvr62@gmail.com>
2025-06-30 17:50:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,43 @@
 
 import time
 import subprocess
+import shelve
+import sys
+import emoji
+
 print('-'*20)
 subprocess.run(["cat","desenho.txt"])
 print('-'*20)
 
-tarefa = input('Adicionar a lista: ')
+def mostrar_lista():
 
-lista = []
-lista.append(tarefa)
+    devagar(f'  ' + 'LISTA'.center(18, '_'))
+    print('#|' + " ".center(18,' ') + '|')
+    with shelve.open('lista') as db:
+        if db['tarefas'] == []:
+            print(f'#|'+'!Nada criado!'.center(18, ' ') + '|')
+
+        for indice, tarefa in enumerate(db['tarefas'], start=1):
+            print(emoji.emojize(f"#|{f'•{indice} - {tarefa}'.center(35)}|"))
+        print('#|' + " ".center(18,' ') + '|')
+        print('#|' + "".center(18,'_') + '|')
+        
+
+def carregamento(titulo='', seg=3):
+    for i in range(seg*4):
+        pontos = '.'*(i % 4)
+        sys.stdout.write(f'\r{titulo}{pontos}')
+        sys.stdout.flush()
+        time.sleep(0.25)
+
+def devagar(texto, delay=0.03):
+    for c in texto:
+        print(c, end='', flush=True)
+        time.sleep(delay)
+    print()
+time.sleep(1)
+print('MANUAL: ')
+devagar(' Digite 4 para ver a lista;\n digite 3 para adicionar a lista;\n digite 2 para remover algo da lista com base em seu numero.')
 
 def menu():
   print('\n')
@@ -18,58 +47,65 @@ def menu():
   print('[3] add item to the lista')
   print('[2] remove itens of lista')
   print('[1] mark an item of lista')
+  print('[0] leave')
 menu()
 
 while True:
-  sistema = int(input('~ '))
+    try:
+        sistema = int(input('menu ~ '))
   
-  if sistema == 4:
-    print('LISTA'.center(20,'-'))
-    for indice,tarefa1 in enumerate(lista, start=1):
-      print(f'{indice} - {tarefa1}')
-    time.sleep(3)
-    menu()
-      
-  elif sistema == 3:
-    sistema1 = input('add: ')
-    lista.append(sistema1)
-    time.sleep(1)
+        if sistema == 4:
+            carregamento(titulo='carregando lista')
+            print('\n')
+            with shelve.open('lista') as db:
+                if 'tarefas' not in db:
+                    print('    !LISTA AINDA NÃO CRIADA!\n    !ADICIONE ALGO!')
+                else:
+                    mostrar_lista()
+                    menu()
+
+        elif sistema == 3:
+            with shelve.open('lista') as db:
+                sistema1 = input('add: ').lower()
+                if not 'tarefas' in db:
+                    db['tarefas'] = []
+                    devagar('LISTA CRIADA!')
+                existe = any(item.split(':')[0] == sistema1 for item in db['tarefas'])
+            
+                if not existe:
+                    db['tarefas'] = db['tarefas'] + [f'{sistema1}::check_mark_button:']
+
+                time.sleep(1)
+                mostrar_lista()
+                time.sleep(3)
+            menu()
     
-    for indice, tarefa1 in enumerate(lista, start=1):
-      print(f'{indice} - {tarefa1}')
-    time.sleep(3)
-    menu()
-    
-  elif sistema == 2:
-    while True:
-      try:
-        sistema2 = int(input('remove: '))
-      except ValueError:
-        print('!Invalid Option!')
-        break
-      except NameError:
-        print('Invalid Option!')
-        break
-      else:
+        elif sistema == 2:
+            while True:
+                try:
+                    sistema2 = int(input('remove: '))
+                except ValueError:
+                    print('!Invalid Option!')
+                else:
+                    remover_item = sistema2 - 1
+                    with shelve.open('lista') as db:            
+                        if 0<= remover_item < len(db['tarefas']):
+                            aswer = input('Are you sure? Y/n\n~ ').strip().lower()
           
+                            if aswer == 'n':
+                                print('CANCELED')
+                                break
           
-        remover_item = sistema2 - 1
-        
-        if 0<= remover_item < len(lista):
-          aswer = input('Are you sure? Y/n\n~ ').strip().lower()
-          
-          if aswer == 'n':
-            print('CANCELED')
+                            elif aswer == 'y':
+                                teste = db['tarefas'].copy()
+                                teste.pop(remover_item)
+                                db['tarefas'] = teste
+                                mostrar_lista()
+                        else:
+                            print('invalid aswer, please! try again')
+                    break
+        elif sistema == 0:
+            carregamento(titulo='saindo', seg=1)
             break
-          
-          elif aswer == 'y':
-            lista.pop(remover_item)
-            for n, item in enumerate(lista, start=1):
-              print(f'{n} - {item}')
-              break
-            break
-          
-          else:
-            print('invalid aswer, please! try again')
-        else:
-          print('Digita certo mano!')
+    except ValueError:
+        print('!Precisa ser um numero do Menu!')
